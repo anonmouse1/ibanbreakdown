2bda834e2ae3255fd9335ac351a8af89da8a684a
anonmouse1 <90865869+anonmouse1@users.noreply.github.com>
2023-12-11 23:45:52 +0100

    Update main.js
    add copy functionality

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
-document
-  .getElementById("ibanForm")
-  .addEventListener("submit", function (event) {
+document.getElementById('ibanForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    const iban = document.getElementById("ibanInput").value;
+    const iban = document.getElementById('ibanInput').value;
     if (isValidIban(iban)) {
-      const { sortCode, accountNumber } = extractSortCodeAndAccountNumber(iban);
-      document.getElementById(
-        "result"
-      ).innerText = `Sort Code: ${sortCode}, Account Number: ${accountNumber}`;
+        const { sortCode, accountNumber } = extractSortCodeAndAccountNumber(iban);
+        const resultText = `Sort Code: ${sortCode}, Account Number: ${accountNumber}`;
+        document.getElementById('result').innerText = resultText;
+        document.getElementById('copyButton').style.display = 'block';
     } else {
-      document.getElementById("result").innerText = "Invalid IBAN.";
+        document.getElementById('result').innerText = "Invalid IBAN.";
+        document.getElementById('copyButton').style.display = 'none';
     }
-  });
+});
+
+document.getElementById('copyButton').addEventListener('click', function() {
+    const resultText = document.getElementById('result').innerText;
+    navigator.clipboard.writeText(resultText).then(() => {
+        alert('Copied to clipboard!');
+    }).catch(err => {
+        console.error('Error in copying text: ', err);
+    });
+});
 
 function isValidIban(iban) {
-  return /^[A-Z]{2}\d{2}[A-Z]{4}[0-9A-Z]{6}[0-9A-Z]{8,16}$/.test(iban);
+    return /^[A-Z]{2}\d{2}[A-Z]{4}[0-9A-Z]{6}[0-9A-Z]{8,16}$/.test(iban);
 }
 
 function extractSortCodeAndAccountNumber(iban) {
-  return {
-    sortCode: iban.substring(8, 14),
-    accountNumber: iban.substring(14, 22),
-  };
+    return {
+        sortCode: iban.substring(8, 14),
+        accountNumber: iban.substring(14, 22)
+    };
 }
